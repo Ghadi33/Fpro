52900e54f187efc33894d7937cbde5df4b55f2f8
= <=>
2024-03-27 18:00:30 +0300

    input form for search & footer

--- myApp/views.py
+++ myApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    template = 'index.html'
-    return render(request, template)
+    return render(request, 'pages/index.html')
 
+def about(request):
+    return render(request, 'pages/about.html')
